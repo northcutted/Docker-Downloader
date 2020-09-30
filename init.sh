f5035f0c0f9034527d570077c0f7540b9022167a
northcutted <northcutted@gmail.com>
2020-09-30 18:24:29 -0500

    first round of testing

---
+++ init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+function setUpWorkspace () {
+    echo_time "Creating Workspace.."
+
+    echo_time "Creating directory structure.."
+    mkdir -p $PATH_TO_DOCKER_CONFIG/init/scripts
+    mkdir -p $PATH_TO_DOCKER_CONFIG/jackett
+    mkdir -p $PATH_TO_DOCKER_CONFIG/pia
+    mkdir -p $PATH_TO_DOCKER_CONFIG/pia-shared
+    mkdir -p $PATH_TO_DOCKER_CONFIG/radarr
+    mkdir -p $PATH_TO_DOCKER_CONFIG/sonarr
+    mkdir -p $PATH_TO_DOCKER_CONFIG/bazarr
+    mkdir -p $PATH_TO_DOCKER_CONFIG/mylar
+    mkdir -p $PATH_TO_DOCKER_CONFIG/transmission
+    echo_time "Creating directory structure...DONE"
+
+    echo_time "Creating Workspace...DONE"
+}
+
+function cleanUp () {
+    echo_time "Stopping containers.."
+    docker stop $(docker ps -aq) > /dev/null
+    echo_time "Stopping containers...DONE"
+
+    echo_time "Removing all containers.."
+    docker rm $(docker ps -aq)
+    echo_time "Removing all containers...DONE"
+
+    echo_time "Removing all images.."
+    docker rmi $(docker images -q)
+    echo_time "Removing all images...DONE"
+}
+
+function startContainers () {
+    echo_time "Updating and starting containers.."
+    cd $PATH_TO_DOCKER_CONFIG/init && docker-compose up -d
+    echo_time "Updating and starting containers...DONE"
+}
+
+
+function init () {
+    oldPort=0
+    if test -f "$PATH_TO_DOCKER_CONFIG/transmission/forwarded_port"; 
+    then
+        oldPort=$(cat $PATH_TO_DOCKER_CONFIG/transmission/forwarded_port)
+    fi
+
+    startContainers
+}
